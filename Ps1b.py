e0f0607d24a8007f090c216a5721949e5120333c
Hexpee <52126266+Hexpee@users.noreply.github.com>
2019-09-20 15:14:50 +0100

    Update Ps1b.py

--- Ps1b.py
+++ Ps1b.py
@@ -1,20 +1,20 @@
 #A program that calculate month it take a user to buy house.there should be increase in Salary if the user works more than some certain month
-annualSalary = float(input("Enter your annual salary: "))
-#portion saved
-portionSaved = float(input("Enter the percent of your salary to save, in decimal: "))
-totalCost = float(input("Enter the cost of your dream home: "))
+annual_salary = float(input("Enter your annual salary: "))
+#portion saved in percentage
+portion_saved = float(input("Enter the percent of your salary to save, in decimal: "))
+total_cost = float(input("Enter the cost of your dream home: "))
 #new Variable for the Question 1b for semi annual raise incrsement....
-semiAnnualRaise = float(input("Enter the semi­annual raise, as a decimal: "))
+semi_annual_raise = float(input("Enter the semi­annual raise, as a decimal: "))
 #to get the portion down payment 
-portionDownPayment = totalCost * 0.25
+portion_down_payment = total_cost * 0.25
 #initializing the current saving getting a return value from monthly salary X by portion saved plus value of current+
-currentSavings = 0
+current_savings = 0
 r = 0.04
 months = 0
-monthlySalary=annualSalary/12
-while currentSavings < portionDownPayment:
-    currentSavings += (monthlySalary)*portionSaved + currentSavings*(r/12)
+monthly_salary=annual_salary/12
+while current_savings < portion_down_payment:
+    current_savings += (monthly_salary)*portion_saved + current_savings*(r/12)
     months += 1
     if months%6 == 0:
-        annualSalary += annualSalary * semiAnnualRaise
+        annual_salary += annual_salary * semi_annual_raise
 print("Number of months to saved: ", months)
